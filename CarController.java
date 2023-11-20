6673dc59b70ab9daca97ce32d25df806df2d2f24
Simon-A-Victor <simonarthurvictor@gmail.com>
2023-11-20 14:14:17 +0100

    Add a saab

--- CarController.java
+++ CarController.java
@@ -29,7 +29,8 @@ public class CarController {
         // Instance of this class
         CarController cc = new CarController();
 
-        cc.cars.add(new Volvo240(0,0));
+        cc.cars.add(new Volvo240(0,800));
+        cc.cars.add(new Saab95(100,800));
 
         // Start a new view and send a reference of self
         cc.frame = new CarView("CarSim 1.0", cc);
