3eb0da7279d291bd03d5c0322aafc2cd237100c2
Oliver <o.anderzen@gmail.com>
2023-12-06 11:40:01 +0100

    create state for movement for Scania class

---
+++ Model/MovementTrueState.java
@@ -0,0 +1,12 @@
+package Model;
+
+public class MovementTrueState extends MovementState {
+    public MovementTrueState(Scania vehicle) {
+        super(vehicle);
+    }
+
+    @Override
+    public void onTiltPlatform(double degrees) {
+
+    }
+}
