b94640f360decc2b47cf6bf8969d04b84ee66ea6
Simon-A-Victor <simonarthurvictor@gmail.com>
2023-12-06 09:40:35 +0100

    Bug fix

--- Model/Directions.java
+++ Model/Directions.java
@@ -1,8 +1,17 @@
 package Model;
 
+import java.util.Random;
+
 public enum Directions {
     NORTH,
     EAST,
     SOUTH,
-    WEST
+    WEST;
+    private static final Random random = new Random();
+
+    public static Directions randomDirection()  {
+        Directions[] directions = values();
+        return directions[random.nextInt(directions.length)];
+    }
+
 }
